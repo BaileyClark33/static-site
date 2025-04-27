4889ea14f15868143aaeb2974b635de7d067cdc0
BaileyClark33 <bwc339@gmail.com>
2025-04-27 15:53:48 -0400

    changed to bootdev files

--- src/main.py
+++ src/main.py
@@ -1,22 +1,9 @@
-from htmlnode import HTMLNode
 from textnode import TextNode, TextType
 
 
 def main():
-    node = TextNode("test", TextType.LINK, "https://www.boot.dev")
-    node2 = HTMLNode(
-        "This is an html node",
-        "<p>",
-        None,
-        {
-            "href": "https://www.google.com",
-            "target": "_blank",
-        },
-    )
-
-    print(node.__repr__())
-    print(node2.__repr__())
-    print(node2.props_to_html())
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
 
 main()
